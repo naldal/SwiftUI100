75a183f2ec38b0758fb4ad849938a56b160a8121
Hamin Song <cec7378@gmail.com>
2023-12-31 22:55:19 +0900

    scrollView with buonce behavior

---
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by 송하민 on 3/3/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    var order: Order
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action: {})
+                    .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -22,4 +22,31 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Decimal {
+        var cost = Decimal(quantity) * 2
+        cost += Decimal(type) / 2
+    
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
 }
