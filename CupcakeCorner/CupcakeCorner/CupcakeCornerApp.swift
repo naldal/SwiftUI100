49af9f13c1dc7e4f867a2ba41c1470abd4d6d3bb
Hamin Song <cec7378@gmail.com>
2024-01-03 15:05:37 +0900

    task with codable

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 송하민 on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
