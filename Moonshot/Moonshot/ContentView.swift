2196c49eee3492ed7398c3c337961295543232b9
Hamin Song <cec7378@gmail.com>
2024-01-10 23:45:50 +0900

    learn lazy stacks

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Image(.armstrong)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
-            }
+        ScrollView (.horizontal){
+            LazyHStack(spacing: 10, content: {
+                ForEach(0..<100) {
+                    CustomText(text: "item \($0)")
+                        .font(.title)
+                }
+            })
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
