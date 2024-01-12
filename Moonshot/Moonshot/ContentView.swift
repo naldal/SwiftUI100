43cc1f822fdb032794eae84a675a70a38ba99140
Hamin Song <cec7378@gmail.com>
2024-01-12 23:53:42 +0900

    learn navigation link

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,28 +7,15 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView (.horizontal){
-            LazyHStack(spacing: 10, content: {
-                ForEach(0..<100) {
-                    CustomText(text: "item \($0)")
-                        .font(.title)
+        NavigationStack {
+            List(0..<100) { row in
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row )")
                 }
-            })
-            .frame(maxWidth: .infinity)
+            }
+            .navigationTitle("swiftui")
         }
     }
 }
