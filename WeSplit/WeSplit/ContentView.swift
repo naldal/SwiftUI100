80e10f75dd0c1c5453efbc8227b02e0561fe3f78
Hamin Song <cec7378@gmail.com>
2024-01-18 15:49:55 +0900

    pickers option

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,20 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"]
-    @State private var selectedStudent = ""
+    
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) poeple")
+                        }
                     }
                 }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            .navigationTitle("Select a Student")
+    
         }
+        .navigationTitle("WeSplit")
+   
     }
 }
 
