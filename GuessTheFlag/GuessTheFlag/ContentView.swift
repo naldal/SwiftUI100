97a78f2185319489b1b79052c7e2d7b7cd6fc4a2
Hamin Song <cec7378@gmail.com>
2024-01-26 15:37:28 +0900

    make flag game

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -27,53 +27,105 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var leftQuestion = 8
+    @State private var isShowingFinalScore: Bool = false
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(
+                stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                ],
+                center: .top,
+                startRadius: 200,
+                endRadius: 700
+            )
+            .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                        .font(.subheadline.weight(.heavy))
+            VStack {
+                Spacer()
+                Text("Guess the FLag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
                     
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
-                
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 40)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Countinue", action: askQuestion)
         } message: {
-            Text("Your score is ??")
+            Text("Your score is \(score)")
+        }
+        .alert("Done!", isPresented: $isShowingFinalScore) {
+            Button("Reset the game", action: resetGame)
+        } message: {
+            Text("Your final score is \(self.score)")
         }
     }
     
     func flagTapped(_ number: Int) {
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
+            self.score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong!\nThat's the flag of \(countries[number])"
         }
         showingScore = true
     }
     
     func askQuestion() {
+        self.leftQuestion -= 1
+        
+        guard self.leftQuestion > 0 else {
+            self.isShowingFinalScore = true
+            self.showingScore = false
+            return
+        }
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        self.isShowingFinalScore = false
+        self.score = 0
+        self.leftQuestion = 8
+        self.askQuestion()
     }
 }
 
