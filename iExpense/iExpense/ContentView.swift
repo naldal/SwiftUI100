04898cd7684bda9a2ce225207289967d47e2c7de
Hamin Song <cec7378@gmail.com>
2024-02-15 21:29:11 +0900

    use @Observable and @State complex

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 
-struct User {
-    let firstName: String
-    let lastName: String
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expense {
+    var items: [ExpenseItem] = []
+    
 }
 
 struct ContentView: View {
     
+    @State private var expenses = Expense()
+    
     var body: some View {
-        VStack {
-            
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems(at:))
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
