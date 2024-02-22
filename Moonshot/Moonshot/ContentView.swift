47a8dad50017add9486b08623b04ab9118800d2a
Hamin Song <cec7378@gmail.com>
2024-02-22 20:27:32 +0900

    make moonshot

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     let columns = [
-        GridItem(.adaptive(minimum: 350))
+        GridItem(.adaptive(minimum: 150))
     ]
     
     var body: some View {
@@ -28,21 +28,34 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundStyle(.white)
                                     
-                                    Text(mission.launchDate ?? "N/A")
+                                    Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
                                 }
                                 .frame(maxWidth: .infinity)
+                                .padding(.vertical)
+                                .background(.lightBackground)
                             }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -16,7 +16,7 @@ struct Mission: Codable, Identifiable {
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     
@@ -26,5 +26,9 @@ struct Mission: Codable, Identifiable {
     
     var image: String {
         "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
 }
