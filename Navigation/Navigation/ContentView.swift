d5817e81eb4763dc17ad36328af006c05057f8a8
Hamin Song <cec7378@gmail.com>
2024-02-27 21:27:07 +0900

    navigationbar appearance

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,61 +7,17 @@
 
 import SwiftUI
 
-class PathScore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-    
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-        path = NavigationPath()
-    }
-    
-    func save() {
-        guard let representaion = path.codable else { return }
-        do {
-            let data = try JSONEncoder().encode(representaion)
-            try data.write(to: savePath)
-
-        } catch {
-            print("failed to save navigation data ")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    @Binding var path: NavigationPath
-    
-    var body: some View {
-        NavigationLink("go to random number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
-                }
-            }
-    }
-}
-
 struct ContentView: View {
-    @State private var path = NavigationPath()
-    
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
-                }
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue, for: .navigationBar)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
 }
